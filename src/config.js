1668fa3fa9c6623356cfc51ffcd50b3a5878199d
Mikhail Golubtsov <mgolubcov@gmail.com>
2021-12-13 00:09:04 +0300

    добавляет ключик

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY; // .env.local // REACT_APP_API_KEY='0735de07-3afaed97-0f67483a-2f86590b'
+// const API_KEY = '0735de07-3afaed97-0f67483a-2f86590b';
 const API_URL = 'https://fortniteapi.io/v1/shop?lang=ru';
 
 export { API_KEY, API_URL };
